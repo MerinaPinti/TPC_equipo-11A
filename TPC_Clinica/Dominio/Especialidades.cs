24fe8dd663039ec5829fe0bd51906a1c02e4fc58
Ailinmino <daianaailin.40@gmail.com>
2025-06-04 16:39:38 -0300

    Commit de prueba

--- TPC_Clinica/Dominio/Especialidades.cs
+++ TPC_Clinica/Dominio/Especialidades.cs
@@ -8,7 +8,7 @@ namespace Dominio
 {
     public class Especialidades
     {
-        
+        //commit de prueba
             public int ID { get; set; }
             public string Nombre { get; set; }
         
