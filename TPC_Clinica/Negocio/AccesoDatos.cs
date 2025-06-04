83a72e1f54a0a7f76273223d87c8bada9f88f120
MerinaPinti <marinacelestepinti2@gmail.com>
2025-06-03 22:14:21 -0300

    Acceso a Datos y Especialidades Negocio

---
+++ TPC_Clinica/Negocio/AccesoDatos.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection(
+                //CONEXIÓN MERI
+                "server=.\\SQLEXPRESS01; database=CLINICA_DB; integrated security=true"
+
+                //CONEXION NORMAL
+                //"server=.\\SQLEXPRESS; database=CLINICA_DB; integrated security=true"
+                );
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public void setearParametros(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void cerrarConexion()
+        {
+            if (lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+
+        public int ejecutarScalar()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                object resultado = comando.ExecuteScalar();
+                return Convert.ToInt32(resultado);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                cerrarConexion();
+            }
+        }
+    }
+}
+
---
+++ TPC_Clinica/Negocio/EspecialidadesNegocio.cs
@@ -0,0 +1,43 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+   public class EspecialidadesNegocio
+    {
+        public List<Especialidades> Listar()
+        {
+            List<Especialidades> lista = new List<Especialidades>();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT ID, Nombre FROM ESPECIALIDADES ORDER BY Nombre");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Especialidades esp = new Especialidades();
+                    esp.ID = (int)datos.Lector["ID"];
+                    esp.Nombre = (string)datos.Lector["Nombre"];
+
+                    lista.Add(esp);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+    }
+
