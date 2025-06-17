e1d0014580b9177a57a44c8b5f7010bfc816d876
MerinaPinti <marinacelestepinti2@gmail.com>
2025-06-17 18:16:41 -0300

    Usuario Negocio
    Funcionalidades listar, modificar y agregar (lista) de Usuarios

--- TPC_Clinica/Negocio/UsuarioNegocio.cs
+++ TPC_Clinica/Negocio/UsuarioNegocio.cs
@@ -11,21 +11,32 @@ namespace Negocio
 {
     public class UsuarioNegocio
     {
-        public void agregarUsuario(Usuario nuevo)
+
+        public List<Usuario> Listar()
         {
+            List<Usuario> lista = new List<Usuario>();
             AccesoDatos datos = new AccesoDatos();
+
             try
             {
-                datos.setearConsulta("INSERT INTO Usuario (idUsuario, idTipoUsuario, usuario, contraseña) values (@TipoUsuario, @UserName, @Password)");
-                datos.setearParametros("@TipoUsuario", nuevo.TipoUsuario);
-                datos.setearParametros("@UserName", nuevo.UserName);
-                datos.setearParametros("@Password", nuevo.Password);
+                datos.setearConsulta("SELECT idTipoUsuario, usuario, contraseña, activo FROM Usuario WHERE activo = 1");
+                datos.ejecutarLectura();
 
-                datos.ejecutarAccion();
+                while (datos.Lector.Read())
+                {
+                    Usuario user = new Usuario();
+                    user.TipoUsuario = datos.Lector["idTipoUsuario"].ToString();
+                    user.UserName = (string)datos.Lector["usuario"];
+                    user.Password = (string)datos.Lector["contraseña"];
+                    user.Activo = (bool)datos.Lector["activo"];
+
+                    lista.Add(user);
+                }
+
+                return lista;
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
             finally
@@ -34,8 +45,85 @@ namespace Negocio
             }
         }
 
+        public void agregarUsuario(List<Usuario> lista)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                foreach (Usuario usuario in lista)
+                {
+                    datos.setearConsulta("INSERT INTO Usuario (usuario, contraseña, idTipoUsuario) VALUES (@user, @pass, @tipo)");
+                    datos.setearParametros("@user", usuario.UserName);
+                    datos.setearParametros("@pass", usuario.Password);
+                    datos.setearParametros("@tipo", int.Parse(usuario.TipoUsuario));
+                    datos.ejecutarAccion();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+
+        public void modificarUsuario(Usuario usuario)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("UPDATE Usuario SET contraseña = @password WHERE idUsuario = @id");
+                datos.setearParametros("@id", usuario.Id);
+                datos.setearParametros("@password", usuario.Password);
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
 
 
+
+
+        }
+
+        public Usuario ListarConId(int id)
+        {
+            Usuario usuario = new Usuario();
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+                datos.setearConsulta("SELECT idUsuario as Id, usuario, contraseña, idTipoUsuario FROM Usuario WHERE idUsuario = @id");
+                datos.setearParametros("@id", id);
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    usuario.Id = (int)datos.Lector["Id"];
+                    usuario.UserName = (string)datos.Lector["usuario"];
+                    usuario.Password = (string)datos.Lector["contraseña"];
+                    usuario.TipoUsuario = datos.Lector["idTipoUsuario"].ToString(); // guardás como string
+                }
+
+                return usuario;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
 
     }
 }
