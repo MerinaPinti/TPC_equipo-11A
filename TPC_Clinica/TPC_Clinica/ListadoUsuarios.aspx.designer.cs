2bb7e2d3b1ef767460c72b2c6a46d85769b9dff0
MerinaPinti <marinacelestepinti2@gmail.com>
2025-06-17 18:18:30 -0300

    Git

--- TPC_Clinica/TPC_Clinica/ListadoUsuarios.aspx.designer.cs
+++ TPC_Clinica/TPC_Clinica/ListadoUsuarios.aspx.designer.cs
@@ -15,12 +15,21 @@ namespace TPC_Clinica
     {
 
         /// <summary>
-        /// Control Label1.
+        /// Control btnAgregar.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label1;
+        protected global::System.Web.UI.WebControls.Button btnAgregar;
+
+        /// <summary>
+        /// Control dgvUsuarios.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView dgvUsuarios;
     }
 }
