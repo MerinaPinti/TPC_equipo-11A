382e9da473f5a359d828d4913c50c890b0b2e9cb
MerinaPinti <marinacelestepinti2@gmail.com>
2025-06-27 17:28:34 -0300

    Seleccionar Profesional
    Filtro para búsqueda.
    Navegación entre páginas OK.

---
+++ TPC_Clinica/TPC_Clinica/SeleccionarProfesional.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPC_Clinica
+{
+    public partial class SeleccionarProfesional : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnContinuar_Click(object sender, EventArgs e)
+        {
+            string filtro = txtBuscarProfesional.Text.Trim();
+
+            if (filtro.Length < 3)
+            {
+                
+                return;
+            }
+
+        }
+
+
+    }
+}
