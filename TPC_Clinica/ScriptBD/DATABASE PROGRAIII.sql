90f445fd1ef73d4c977a250b42fcc39cd488546f
Johannes Kalksma <johygogo@gmail.com>
2025-06-26 22:07:38 -0300

    Se modifica script de la BD para manejar turnos de trabajo y horarios

--- TPC_Clinica/ScriptBD/DATABASE PROGRAIII.sql
+++ TPC_Clinica/ScriptBD/DATABASE PROGRAIII.sql
@@ -59,14 +59,22 @@ CREATE TABLE Medico(
 	telefono VARCHAR(15),
 	nombre VARCHAR(100),
 	apellido VARCHAR(100),
-	matricula VARCHAR(90) NOT NULL UNIQUE,
-	idEspecialidad INT FOREIGN KEY REFERENCES Especialidad(idEspecialidad),
-	idUsuario INT UNIQUE FOREIGN KEY REFERENCES Usuario(idUsuario),
-	activo BIT NOT NULL DEFAULT 1 -- 1: activa, 0: inactiva
+	matricula VARCHAR(90) NOT NULL UNIQUE, 
+	idUsuario INT NULL, 
+	activo BIT NOT NULL DEFAULT 1,
+	FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario)
 );
 
 GO
 
+CREATE TABLE Especialidades_Medicos (
+	idEspecialidad INT NOT NULL FOREIGN KEY REFERENCES Especialidad(idEspecialidad),
+    idMedico INT NOT NULL FOREIGN KEY REFERENCES Medico(idMedico),
+	Activo bit NOT NULL DEFAULT 1,
+    PRIMARY KEY(idEspecialidad, idMedico)
+);
+
+GO
 CREATE TABLE Turno(
 	idTurno INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	idPaciente INT FOREIGN KEY REFERENCES Paciente(idPaciente),
@@ -81,51 +89,19 @@ CREATE TABLE Turno(
 	activo BIT NOT NULL DEFAULT 1 -- 1: activa, 0: inactiva
 );
 
-GO
+CREATE TABLE TurnoTrabajo (
+	idTurnoTrabajo INT PRIMARY KEY IDENTITY(1,1),
+	descripcion VARCHAR(50) NOT NULL,
+	horaInicio TIME NOT NULL,
+	horaFin TIME NOT NULL,
+	activo BIT NOT NULL DEFAULT 1
+);
 
--- Tabla TipoUsuario
-INSERT INTO TipoUsuario (Descripcion) VALUES
-('Administrador'),
-('Médico'),
-('Recepcionista');
-
--- Tabla Usuario
-INSERT INTO Usuario (idTipoUsuario, Usuario, Contraseña) VALUES
-(1, 'admin01', 'passAdmin123'),
-(2, 'medico01', 'passMedico123'),
-(3, 'recep01', 'passRecep123');
--- Tabla Estado
-
-INSERT INTO Estado (Descripcion) VALUES
-('Nuevo'),
-('Confirmado'),
-('Reprogramado'),
-('Cancelado'),
-('No Asistió'),
-('Cerrado');
-
--- Tabla Especialidad
-INSERT INTO Especialidad (Descripcion) VALUES
-('Cardiología'),
-('Dermatologpia'),
-('Endocrinología'),
-('Ginecología'),
-('Hematología'),
-('Nefrología'),
-('Obstetricia'),
-('Oftalmología'),
-('Otorrinolaringología'),
-('Pediatría'),
-('Traumatología');
-
--- Tabla Paciente
-INSERT INTO Paciente (Nombre, Apellido, DNI, FechaNac, Telefono, Direccion, Email) VALUES
-('Juan', 'Pérez', '12345678', '1980-05-12', '1122334455', 'Av. Siempre Viva 123', 'juan.perez@mail.com'),
-('María', 'González', '87654321', '1990-10-25', '1144556677', 'Calle Falsa 456', 'maria.gonzalez@mail.com'),
-('Carlos', 'López', '11223344', '1975-03-08', '1133557799', 'Pasaje 123', 'carlos.lopez@mail.com');
-
--- Tabla Medico
-INSERT INTO Medico (Email, Telefono, Nombre, Apellido, Matricula, idEspecialidad, idUsuario) VALUES
-('alejandro.ramos@clinica.com', '1199887766', 'Alejandro', 'Ramos', '654321', 1, 1),
-('laura.sanchez@clinica.com', '1177665544', 'Laura', 'Sánchez', '765432', 2, 2),
-('fernando.martinez@clinica.com', '1166554433', 'Fernando', 'Martínez', '876543', 3, 3);
+CREATE TABLE HorarioAtencion (
+	idHorarioAtencion INT PRIMARY KEY IDENTITY(1,1),
+	idMedico INT NOT NULL FOREIGN KEY REFERENCES Medico(idMedico),
+	idEspecialidad INT NOT NULL FOREIGN KEY REFERENCES Especialidad(idEspecialidad),
+	idTurnoTrabajo INT NOT NULL FOREIGN KEY REFERENCES TurnoTrabajo(idTurnoTrabajo),
+	diaSemana TINYINT NOT NULL CHECK(diaSemana BETWEEN 1 AND 7), -- 1 = lunes, 7 = domingo
+	activo BIT NOT NULL DEFAULT 1
+);
