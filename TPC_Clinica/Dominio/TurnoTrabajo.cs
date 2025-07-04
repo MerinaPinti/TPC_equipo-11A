f831f44c980e5b0679ea2c83eab48210920ccd00
MerinaPinti <marinacelestepinti2@gmail.com>
2025-07-04 17:51:43 -0300

    Creacion Clase Turno Trabajo

---
+++ TPC_Clinica/Dominio/TurnoTrabajo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class TurnoTrabajo
+    {
+
+        public int IdTurnoTrabajo { get; set; }
+        public string Descripcion { get; set; }
+        public TimeSpan HoraInicio { get; set; }
+        public TimeSpan HoraFin { get; set; }
+        public bool Activo { get; set; }
+    }
+}
