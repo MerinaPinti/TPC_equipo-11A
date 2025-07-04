b614fc5a3adf14fc702155ef4dfbccda73ce599e
MerinaPinti <marinacelestepinti2@gmail.com>
2025-07-04 17:51:28 -0300

    Creación Clase Horario Atencion

---
+++ TPC_Clinica/Dominio/HorarioAtencion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class HorarioAtencion
+    {
+        public int IdHorarioAtencion { get; set; }
+        public int IdMedico { get; set; }
+        public int IdEspecialidad { get; set; }
+        public int IdTurnoTrabajo { get; set; }
+        public int DiaSemana { get; set; } 
+        public bool Activo { get; set; }
+    }
+}
