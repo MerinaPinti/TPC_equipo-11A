d30275c2d846ccf71532688c01366d6eeb2edb63
Ailinmino <daianaailin.40@gmail.com>
2025-07-09 19:10:20 -0300

    HISTORIACLINICA(front)
    se crea pantalla para buscar historia clinica de los pacientes por DNI

---
+++ TPC_Clinica/TPC_Clinica/HistoriaClinica.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Master.Master" AutoEventWireup="true" CodeBehind="HistoriaClinica.aspx.cs" Inherits="TPC_Clinica.HistoriaClinica" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+<div class="container mt-4">
+        <h3>Historial Clinica</h3>
+
+        <div class="row g-3">
+            <div class="col-md-4">
+                <label for="txtDNI">DNI del paciente:</label>
+                <asp:TextBox ID="txtDNI" runat="server" CssClass="form-control" />
+            </div>
+
+            <div class="col-md-4">
+                <label for="ddlMedico">Médico:</label>
+                <asp:DropDownList ID="ddlMedico" runat="server" CssClass="form-control" />
+            </div>
+
+            <div class="col-md-4">
+                <label for="ddlEspecialidad">Especialidad:</label>
+                <asp:DropDownList ID="ddlEspecialidad" runat="server" CssClass="form-control" />
+            </div>
+            <asp:Button ID="btnBuscar" runat="server" Text="Buscar" CssClass="btn btn-primary mt-3" OnClick="btnBuscar_Click" />
+        </div>
+
+        <asp:Label ID="lblSinTurnos" runat="server" CssClass="text-danger d-block mt-2" Text="🔍 No se encontraron turnos asignados al paciente."></asp:Label>
+
+
+        <asp:GridView ID="gvResultados" runat="server" CssClass="table table-striped mt-4" AutoGenerateColumns="false">
+            <Columns>
+                <asp:BoundField DataField="Fecha" HeaderText="Fecha" />
+                <asp:BoundField DataField="Medico" HeaderText="Médico" />
+                <asp:BoundField DataField="Especialidad" HeaderText="Especialidad" />
+                <asp:BoundField DataField="Observaciones" HeaderText="Observaciones" />
+                <asp:BoundField DataField="Diagnostico" HeaderText="Diagnostico" />
+            </Columns>
+        </asp:GridView>
+    </div>
+</asp:Content>
---
+++ TPC_Clinica/TPC_Clinica/HistoriaClinica.aspx.designer.cs
@@ -0,0 +1,71 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPC_Clinica
+{
+
+
+    public partial class HistoriaClinica
+    {
+
+        /// <summary>
+        /// Control txtDNI.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDNI;
+
+        /// <summary>
+        /// Control ddlMedico.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlMedico;
+
+        /// <summary>
+        /// Control ddlEspecialidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEspecialidad;
+
+        /// <summary>
+        /// Control btnBuscar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnBuscar;
+
+        /// <summary>
+        /// Control lblSinTurnos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSinTurnos;
+
+        /// <summary>
+        /// Control gvResultados.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvResultados;
+    }
+}
