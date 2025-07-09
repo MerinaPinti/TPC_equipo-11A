dd1bfdca5270aebd873a0a09772411be689c0b37
Ailinmino <daianaailin.40@gmail.com>
2025-07-09 01:32:02 -0300

    CAMBIARCLAVEDEUSUARIO(back)

--- TPC_Clinica/TPC_Clinica/CambiarClaveDeUsuario.aspx.cs
+++ TPC_Clinica/TPC_Clinica/CambiarClaveDeUsuario.aspx.cs
@@ -13,12 +13,68 @@ namespace TPC_Clinica
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-           
+            if (Session["usuario"] == null)
+            {
+                Session["error"] = "Debe iniciar sesión para acceder a esta página.";
+                Response.Redirect("Error.aspx", true);
+            }
+            Session["paginaAnterior"] = System.IO.Path.GetFileName(Request.Url.AbsolutePath);
+
+            if (!IsPostBack)
+            {
+                lblActual.Text = "";
+                lblNueva.Text = "";
+
+                lblActual.Visible = false;
+                lblNueva.Visible = false;
+            }
         }
 
         protected void btnCambiar_Click(object sender, EventArgs e)
         {
-            
+            string actual = txtActual.Text.Trim();
+            string nueva = txtNueva.Text.Trim();
+            string confirmar = txtConfirmar.Text.Trim();
+
+            Usuario usuarioActual = Session["usuario"] as Usuario;
+            //No inició sesión
+            if (usuarioActual == null)
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(actual) || string.IsNullOrEmpty(nueva) || string.IsNullOrEmpty(confirmar))
+            {
+                lblActual.Text = "Completa todos los campos.";
+                lblActual.ForeColor = System.Drawing.Color.Red;
+                lblActual.Visible = true;
+                return;
+            }
+
+            if (nueva != confirmar)
+            {
+                lblNueva.Text = "La nueva contraseña no coincide con su confirmación.";
+                lblNueva.ForeColor = System.Drawing.Color.Red;
+                lblNueva.Visible = true;
+                return;
+            }
+
+            if (actual != usuarioActual.Password) 
+            {
+                lblActual.Text = "La contraseña actual es incorrecta.";
+                lblActual.ForeColor = System.Drawing.Color.Red;
+                lblActual.Visible = true;
+                return;
+            }
+
+            usuarioActual.Password = nueva;
+            UsuarioNegocio negocio = new UsuarioNegocio();            
+            negocio.modificarUsuario(usuarioActual);
+
+            lblActual.Text = "¡Contraseña actualizada correctamente!";
+            lblActual.ForeColor = System.Drawing.Color.Green;
+            lblActual.Visible = true;
         }
     }
 }
